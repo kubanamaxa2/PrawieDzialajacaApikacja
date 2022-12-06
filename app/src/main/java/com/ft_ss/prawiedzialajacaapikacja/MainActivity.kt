cdb01cd6479e6bf1b4ee8d9972b35d811d647088
Dominos0321 <dsch792@gmail.com>
2022-12-06 09:39:47 +0100

    Not innitial commit

--- app/src/main/java/com/ft_ss/prawiedzialajacaapikacja/MainActivity.kt
+++ app/src/main/java/com/ft_ss/prawiedzialajacaapikacja/MainActivity.kt
@@ -2,13 +2,18 @@ package com.ft_ss.prawiedzialajacaapikacja
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RadioButton
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var rb1 = findViewById<RadioButton>(R.id.rb1);
+        var rb2 = findViewById<RadioButton>(R.id.rb2);
+        var Zatwierdz = findViewById<Button>(R.id.Zatwierdz);
 
-        
+
     }
 }
