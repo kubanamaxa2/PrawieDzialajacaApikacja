5728f4e7cc1ad859d36ea6c4ea95d8b209fe61b8
Dominos0321 <dsch792@gmail.com>
2022-12-09 14:10:08 +0100

    Naprawde ostatni commit

--- app/src/main/java/com/ft_ss/prawiedzialajacaapikacja/MainActivity.kt
+++ app/src/main/java/com/ft_ss/prawiedzialajacaapikacja/MainActivity.kt
@@ -9,6 +9,7 @@ import android.widget.Button
 import android.widget.CalendarView
 import android.widget.TextView
 import androidx.core.view.get
+import java.text.SimpleDateFormat
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.util.Calendar
@@ -27,6 +28,13 @@ class MainActivity : AppCompatActivity() {
         val text = findViewById<TextView>(R.id.textView)
         var DataWy = mutableListOf<Int>(0,0,0);
         var DataPrzy = mutableListOf<Int>(1000000,100000,100000);
+        val f1 = SimpleDateFormat("yyyy")
+        val f2 = SimpleDateFormat("MM")
+        val f3 = SimpleDateFormat("dd")
+        Date[0] = f1.format(Date()).toInt()
+        Date[1] = f2.format(Date()).toInt() -1
+        Date[2] = f3.format(Date()).toInt()
+        var DateDis = mutableListOf<Int>(Date[0], Date[1], Date[2])
             calendar.setOnDateChangeListener(
             CalendarView.OnDateChangeListener { view, year, month, dayOfMonth ->
                 Date[0] = year
@@ -38,12 +46,22 @@ class MainActivity : AppCompatActivity() {
                 var roznicamies = (DataPrzy[1] - DataWy[1]) * 31
                 var roznicadni = DataPrzy[2] - DataWy[2]
                 var roznicawsumiedni = roznicalat + roznicamies + roznicadni
+            var roznicalat2 = (DataWy[0] - DateDis[0]) * 365
+            var roznicamies2 = (DataWy[1] - DateDis[1]) * 31
+            var roznicadni2 = DataWy[2] - DateDis[2]
+            var roznicawsumiedni2 = roznicalat2 + roznicamies2 + roznicadni2
             if(DataWy[1]<=0 && (DataPrzy[1]>12 || DataPrzy[1]<0)) {
                 text.text = "Wybierz daty!!"
             }
             else
             {
-                if(roznicawsumiedni <0){
+                if(roznicawsumiedni2 < 0){
+                    text.text = "Wycieczka nie moźe rozpoczynać się w przeszłości"
+                }
+                else if(DataPrzy[0] == 1000000) {
+                    text.text = "Ustaw date przyjazdu"
+                }
+                else if(roznicawsumiedni <0){
                     text.text = "Data Wyjazdu nie może być po dacie przujazdu"
                 }
                 else if(roznicawsumiedni > 730)
@@ -56,7 +74,7 @@ class MainActivity : AppCompatActivity() {
             }
 
         }
-        zatwierdz.setOnClickListener{
+        zatwierdz.setOnClickListener {
             if(rb1.isChecked){
                 licz += 1
                     for (i in 0..2) {
